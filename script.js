3afb5b4b0fa217bfeeb37933d322737569a324ba
Guilherme <guillerplr@gmail.com>
2024-04-18 10:17:18 -0300

    operações funcionando e clique animado

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const calculadora = document.querySelector('.calculadora');
-
 const display = document.querySelector('.display');
-
 const teclas = document.querySelectorAll('.tecla');
 
 let decimal = false;
@@ -10,7 +8,7 @@ let atual = [];
 
 function toStorage(value, operator) {
   if (value != '' && !final) {
-    let atual = JSON.parse(localStorage.getItem('values'));
+    atual = JSON.parse(localStorage.getItem('values'));
     if (!atual) {
       atual = [];
     }
@@ -20,24 +18,53 @@ function toStorage(value, operator) {
     if (atual.length > 3) {
       atual.pop();
       calculadora.setAttribute('data-content', atual.join(' ') + ' =');
-
       finaliza();
     } else {
+      display.value = '';
       calculadora.setAttribute('data-content', atual.join(' '));
     }
   }
 }
 
 function finaliza() {
+  switch (atual[1]) {
+    case '+':
+      display.value = Number(atual[0]) + Number(atual[2]);
+      break;
+    case '-':
+      display.value = Number(atual[0]) - Number(atual[2]);
+      break;
+    case 'x':
+      display.value = Number(atual[0]) * Number(atual[2]);
+      break;
+    case '÷':
+      display.value = Number(atual[0]) / Number(atual[2]);
+      break;
+    default:
+      display.value('Erro ao finalizar');
+      break;
+  }
+
   final = true;
-  console.log('finalize');
+}
+
+function animateClick(target) {
+  target.classList.add('animate');
+  setTimeout(() => {
+    target.classList.remove('animate');
+  }, [100]);
 }
 
 function handleClick({ target }) {
+  animateClick(target);
+
   switch (target.innerHTML) {
-    case ',':
+    case '.':
       if (!decimal) {
-        if (display.value.length != 0) {
+        if (display.value.length === 0) {
+          display.value += '0' + target.innerHTML;
+          decimal = true;
+        } else {
           display.value += target.innerHTML;
           decimal = true;
         }
@@ -55,36 +82,33 @@ function handleClick({ target }) {
       break;
     case '+':
       toStorage(display.value, '+');
-      display.value = '';
       decimal = false;
       break;
     case '-':
       toStorage(display.value, '-');
-      display.value = '';
       decimal = false;
       break;
     case '÷':
       toStorage(display.value, '÷');
-      display.value = '';
       decimal = false;
       break;
     case 'x':
       toStorage(display.value, 'x');
-      display.value = '';
       decimal = false;
       break;
     case '=':
-      toStorage(display.value, '=');
-      display.value = 'resultado';
+      if (localStorage.getItem('values')) {
+        toStorage(display.value, '=');
+      }
       break;
     default:
       if (!final) {
         display.value += target.innerHTML;
       } else {
+        localStorage.clear();
         calculadora.setAttribute('data-content', '');
         display.value = target.innerHTML;
         final = false;
-        localStorage.clear();
       }
       break;
   }
